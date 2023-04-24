d0aec2a69e6eb142131f769eaf6da72747bbb369
matbol <48551028+matbol@users.noreply.github.com>
2023-04-24 15:33:28 +0200

    Change PWM Calculation
    * To account for integer truncation, multiply first, then divide when performing calculations
    * Change the calculation order accordingly

--- heat_ctrl.c
+++ heat_ctrl.c
@@ -50,7 +50,7 @@ int check_heatpower (int heat)
 int heat2pwm (int heat)
 {
   heat = check_heatpower(heat2);
-  return heat / MAX_LOAD_POWER * PWM_RANGE;  
+  return heat * PWM_RANGE / MAX_LOAD_POWER;  
 }
 
 
