5f1ce94ef551941ad0f04bed57042b24b72de59d
matbol <matthias.bolesta@unitybox.de>
2023-05-02 20:54:28 +0200

    Add Teststring for Json Input

--- heat_ctrl.c
+++ heat_ctrl.c
@@ -1,10 +1,15 @@
 
 /*
-In Abh√§ngigkeit der gemessenen Leistung soll die Hardware PWM die entsprechende Pulsweite einstellen. 
+In Abh‰ngigkeit der gemessenen Leistung soll die Hardware PWM die entsprechende Pulsweite einstellen.
 Die PWM muss mit einer festen Frequenz zwischen 1kHz - 10kHz laufen - Vorgabe des Leistungsmoduls.
 Der Heizstab hat eine maximale Leistung von 3 kW.
 
 
+*/
+
+/**
+*
+* Testjson: {"version":"0.3","data":{"tuples":[[1682965855511,468,1]],"uuid":"cba86870-dd59-11ed-81fe-8b6b00f83eed","from":1682965854509,"to":1682965855511,"min":[1682965855511,468],"max":[1682965855511,468],"average":468,"consumption":0.13,"rows":2}}
 */
 
 #include <wiringPi.h>
@@ -24,16 +29,17 @@ Der Heizstab hat eine maximale Leistung von 3 kW.
 void pwm_setup(void);
 int check_heatpower (int heat);
 int heat2pwm (int heat);
+char actual_power_json[] = "{\"version\":\"0.3\",\"data\":{\"tuples\":[[1682965855511,468,1]],\"uuid\":\"cba86870-dd59-11ed-81fe-8b6b00f83eed\",\"from\":1682965854509,\"to\":1682965855511,\"min\":[1682965855511,468],\"max\":[1682965855511,468],\"average\":468,\"consumption\":0.13,\"rows\":2}}"
 
 void pwm_setup(void)
 {
   if (wiringPiSetupGpio() == -1)
     exit (1) ;
-    
+
   pinMode (PWM_PIN01, PWM_OUTPUT);
   pwmSetClock (MAIN_FREQ / PWM_RANGE / PWM_CLK);
   pwmSetRange (PWM_RANGE);
- 
+
   return;
 }
 
@@ -50,7 +56,7 @@ int check_heatpower (int heat)
 int heat2pwm (int heat)
 {
   heat = check_heatpower(heat);
-  return heat * PWM_RANGE / MAX_LOAD_POWER;  
+  return heat * PWM_RANGE / MAX_LOAD_POWER;
 }
 
 
