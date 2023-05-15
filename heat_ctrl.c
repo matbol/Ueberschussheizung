c44023e332f98491049eae3cd7a00f4b34b86f90
matbol <matthias.bolesta@unitybox.de>
2023-05-15 16:30:37 +0200

    Add JSON Routines

--- heat_ctrl.c
+++ heat_ctrl.c
@@ -30,7 +30,6 @@ Der Heizstab hat eine maximale Leistung von 3 kW.
 void pwm_setup(void);
 int check_heatpower (int heat);
 int heat2pwm (int heat);
-char actual_power_json[] = "{\"version\":\"0.3\",\"data\":{\"tuples\":[[1682965855511,468,1]],\"uuid\":\"cba86870-dd59-11ed-81fe-8b6b00f83eed\",\"from\":1682965854509,\"to\":1682965855511,\"min\":[1682965855511,468],\"max\":[1682965855511,468],\"average\":468,\"consumption\":0.13,\"rows\":2}}";
 
 void pwm_setup(void)
 {
@@ -60,15 +59,69 @@ int heat2pwm (int heat)
   return heat * PWM_RANGE / MAX_LOAD_POWER;
 }
 
+char *json_str14 = "{\"version\":\"0.3\",\"data\":{\"tuples\":[[1682965855511,468,1]],\"uuid\":\"cba86870-dd59-11ed-81fe-8b6b00f83eed\",\"from\":1682965854509,\"to\":1682965855511,\"min\":[1682965855511,468],\"max\":[1682965855511,468],\"average\":468,\"consumption\":0.13,\"rows\":2}}";
+
+static bool flag1, flag2;
+static int count;
+static int intstore[4], intcount;
+static int tuples[3];
+int status =0;
+static int tuples_count;
+
+static char json_inner_name_string_dst[256];
+static int inner_value;
+
+//static const char *json_str14 = "{\"name\":\"wobble\",\"value\":{\"inner\":23}}";
+const struct json_attr_t json_inner_int_value[] = {
+    {"tuples", t_integer,
+         .addr.integer = &inner_value},
+    {NULL},
+};
+
+///
+static const struct json_attr_t json_inner_att[] = {
+    {"tuples",      t_array,	.addr.array.element_type = t_integer,
+				.addr.array.arr.integers = tuples,
+				.addr.array.maxlen = 3,
+				.addr.array.count = &tuples_count
+    }
+};
+
+
+static const struct json_array_t json_array_11 = {
+    .element_type = t_integer,
+    .arr.integers.store = intstore,
+    .count = &intcount,
+    .maxlen = sizeof(intstore)/sizeof(intstore[0]),
+};
+
+
+static const struct json_attr_t json_object_14[] = {
+    {"data", t_string,
+        .addr.string = json_inner_name_string_dst,
+        .len = sizeof(json_inner_name_string_dst)},
+    {"tuples", t_array,
+         //.addr.attrs = json_inner_int_value},
+    	.addr.attrs = json_inner_att},
+	{NULL},
+    {"uuid", t_ignore},
+    {"from", t_ignore},
+    {"to", t_ignore},
+    {"min", t_ignore},
+    {"max", t_ignore},
+    {"average", t_ignore},
+    {"consumption", t_ignore},
+    {"rows", t_ignore}
+};
+
 
 
 int main()
 {
-  pwm_setup();
-  for (;;)
-    {
-//      printf("%i\n", index);
-      pwmWrite(PWM_PIN01, 200);
-    }
+  status = json_read_object(json_str14, json_object_14, NULL);
+ 
+//  json_read_array(json_str14, &json_array_11, NULL);
+  printf("%i\n%s\n", tuples[1], status);
+
   return 0;
 }
